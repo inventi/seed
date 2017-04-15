fd3a27291ee1fc0724602e9e7019ce537ae847e6
Zilvinas Kybartas <zhilvis@inventi.lt>
2017-04-15 21:50:04 +0300

    add some events to coffe domain

--- src/seed/coffe/customer.clj
+++ src/seed/coffe/customer.clj
@@ -1,3 +1,19 @@
-(ns seed.coffe.customer)
+(ns seed.coffe.customer
+  (:require [seed.core.command :as command :refer [cmd-error]]
+            [seed.core.aggregate :as aggregate]
+            [seed.core.util :refer [success]]))
 
-(defrecord ConfirmPurchase [])
+(defrecord ConfirmPurchase [merchant])
+(defrecord PurchaseConfirmed [])
+
+(extend-protocol aggregate/Aggregate
+  PurchaseConfirmed
+  (state [event state]
+    (assoc state
+           :state :confirmed
+           :merchant (:merchant event))))
+
+(extend-protocol command/CommandHandler
+  ConfirmPurchase
+  (perform [command state]
+    (success [(map->PurchaseConfirmed command)])))
--- src/seed/coffe/merchant.clj
+++ src/seed/coffe/merchant.clj
@@ -1,5 +1,16 @@
-(ns seed.coffe.merchant)
+(ns seed.coffe.merchant
+  (:require [seed.core.command :as command :refer [cmd-error]]
+            [seed.core.aggregate :as aggregate]
+            [seed.core.util :refer [success]]))
 
-(defrecord SubmitTransaction [])
-(defrecord AproveTransaction [])
+
+(defrecord ConfirmPurchase [customer])
+(defrecord PurchaseConfirmed [])
+
+(defrecord SubmitTransaction [customer amount])
+(defrecord AproveTransaction [customer customer-exist])
 (defrecord RejectTransaction [])
+
+(defrecord TransactionSubmited [])
+(defrecord TransactionAproved [])
+(defrecord TransactionRejected [])
